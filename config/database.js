b581eaab199bcf97848982b7906b130f2c65d0b3
Carlos <carlosalcerrolainez2017@gmail.com>
2023-11-24 23:49:47 -0600

    first commit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.HOST,
-  port: process.env.PORT,
+  port: process.env.PORT_BASE,
   username: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
