9eac71303b5ce2d01b71cff950ee1aa38d14e941
Carlos <carlosalcerrolainez2017@gmail.com>
2023-11-25 00:25:32 -0600

    first commit

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,18 @@
 const cors = require('cors');
-
+const dotenv=require("dotenv")
 // Configuración de opciones de CORS
+dotenv.config();
+const whitelist = [process.env.FRONTEND_URL]
+
 const corsOptions = {
   origin: (origin, callback) => {
-    callback(null, true);
+    if (!origin || whitelist.includes(origin)) {
+      // Puede Consultar la API
+      callback(null, true);
+  } else {
+      // No esta Permitido
+      callback(new Error('Error de Cors'));
+  }
   }, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
