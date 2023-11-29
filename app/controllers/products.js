690dec74a58b91384b57c02f6820a004bda0bda7
Carlos <carlosalcerrolainez2017@gmail.com>
2023-11-28 21:49:29 -0600

    first commit

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -38,14 +38,15 @@ exports.getAllProducts = async (req, res) => {
     // Obtiene todos los productos
     const products = await Product.findAll();
 
-    if (products.length === 0) {
-      return res.status(404).json({ message: 'No se encontraron productos.' });
-    }
+    if(!products) {
+      return res
+      .status(400)
+      .json({message: "There are no products"})
+      }
 
     res.status(200).json({ products });
   } catch (error) {
-    console.error('Error al obtener todos los productos:', error);
-    res.status(500).json({ error: 'Parece haber un problema al obtener todos los productos.' });
+    res.status(500).json({error});
   }
 };
 
