64fa290db19cfbf401e3a726d067d4a034231892
Carlos <carlosalcerrolainez2017@gmail.com>
2023-11-30 01:25:20 -0600

    first commit

--- config/database.js
+++ config/database.js
@@ -3,6 +3,10 @@ const dotenv=require("dotenv");
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2884b77e0ea14003a01171204403e1bbfa4ee015
   host: process.env.HOST,
   port: process.env.PORT,
   username: process.env.USER,
