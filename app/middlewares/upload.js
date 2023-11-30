1bf516c9c1e5db1c135418e3a6d2c855722e6bbb
Carlos <carlosalcerrolainez2017@gmail.com>
2023-11-30 02:12:31 -0600

    first commit

--- app/middlewares/upload.js
+++ app/middlewares/upload.js
@@ -1,32 +1,36 @@
 const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    const pathStorage = `public/images`;
-    cb(null, pathStorage);
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'imagenesfiestisimo', // Specify your Cloudinary folder name
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+    // You can add more Cloudinary parameters as needed
   },
-  filename: function(req, file, cb) {
-    const ext = file.originalname.split(".").pop();
-    const fileName = `file-${req.body.name.replace(/\s+/g, '_')}.${ext}`;
-    cb(null, fileName);
-  }
 });
 
 const upload = multer({
-  storage,
-  fileFilter,
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
   limits: {
-    fieldSize: 25 * 1024 * 1024,
+    fileSize: 25 * 1024 * 1024, // Adjust the file size limit as needed
   },
 });
 
 module.exports = upload;
+
 
